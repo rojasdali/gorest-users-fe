4f6904d7686cb218e31514a4535e78976100e987
Dali Rojas <rojasdali@gmail.com>
2025-03-23 02:52:18 -0400

    feat(config): configure application with authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { provideZoneChangeDetection } from '@angular/core';
 
 import { routes } from '@app/app.routes';
+
 import { authInterceptor } from '@core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
