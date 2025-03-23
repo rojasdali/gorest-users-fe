880d6e41c1dd9e01b72c7a69b5e37f5c481c4326
Dali Rojas <rojasdali@gmail.com>
2025-03-23 13:35:08 -0400

    Trigger Vercel deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: import.meta.env['NG_APP_API_URL'] || process.env['API_URL'],
-  apiToken: import.meta.env['NG_APP_API_TOKEN'] || process.env['API_TOKEN'],
+  production: false,
+  apiUrl: 'https://gorest.co.in/public/v2',
+  apiToken: '1d8e443749f3db497e72b3602c40899c18cedfc8af4c21f28d3ce99a3fd0b544',
 };
