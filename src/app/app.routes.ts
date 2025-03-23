c4f111b529cfa543944d74cbb60ec1c3428a6653
Dali Rojas <rojasdali@gmail.com>
2025-03-23 02:51:45 -0400

    feat(routing): configure protected routes and add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 
-import { UserDetailsComponent } from './features/users/components/user-details/user-details.component';
-import { UserListComponent } from './features/users/components/user-list/user-list.component';
+import { LoginComponent } from '@features/auth/login/login.component';
+import { UserDetailsComponent } from '@features/users/components/user-details/user-details.component';
+import { UserListComponent } from '@features/users/components/user-list/user-list.component';
+
+import { NotFoundComponent } from '@core/components/not-found/not-found.component';
+import { authGuard } from '@core/guards/auth.guard';
+
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
@@ -9,9 +14,18 @@ export const routes: Routes = [
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: '', redirectTo: 'users', pathMatch: 'full' },
-      { path: 'users', component: UserListComponent },
-      { path: 'users/:id', component: UserDetailsComponent },
+      { path: 'login', component: LoginComponent },
+      {
+        path: '',
+        canActivate: [authGuard],
+        children: [
+          { path: '', redirectTo: 'users', pathMatch: 'full' },
+          { path: 'users', component: UserListComponent },
+          { path: 'users/:id', component: UserDetailsComponent },
+        ],
+      },
+      { path: '404', component: NotFoundComponent },
+      { path: '**', redirectTo: '404' },
     ],
   },
 ];
---
+++ src/app/core/components/not-found/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MaterialModule } from '@shared/material.module';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [RouterLink, MaterialModule],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {}
