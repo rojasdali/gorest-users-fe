d33fc41d0d72db632fbce156c18ff4911f46814f
Dali Rojas <rojasdali@gmail.com>
2025-03-23 13:24:59 -0400

    update envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gorest.co.in/public/v2 ',
+  apiUrl: 'https://gorest.co.in/public/v2',
   apiToken: '1d8e443749f3db497e72b3602c40899c18cedfc8af4c21f28d3ce99a3fd0b544',
 };
